1766c2485c0082f45f982bb69da768fe587da784
kabatp <github.tzpmz@passmail.net>
2024-10-01 18:54:07 +0200

    ReCaptcha fix

--- send_email.php
+++ send_email.php
@@ -1,4 +1,4 @@
-php <?php
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Získanie hodnôt z formulára
     $name = htmlspecialchars(trim($_POST['name']));
@@ -14,38 +14,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // reCAPTCHA secret key a odpoveď od klienta
     $recaptchaSecret = '6LfXvE8qAAAAAGDNHzk6wDQ8f1PgUzVoq3HkMv9T';
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    $recaptchaResponse = $_POST['recaptchaResponse'];
 
     // Pošleme požiadavku na Google reCAPTCHA API na overenie
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecret&response=$recaptchaResponse");
-    $responseKeys = json_decode($response, true);
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$recaptchaSecret."&response=".$recaptchaResponse."&remoteip=".$ip;
+    $request = file_get_contents(($url));
+    $response = json_decode($request);
 
-print_r($responseKeys);
+    //print_r($responseKeys);
 
 
     // Skontrolujeme, či reCAPTCHA bola úspešná
-    if (intval($responseKeys["success"]) !== 1) {
-        echo "Please complete the CAPTCHA.";
-        http_response_code(400);
-        exit;
-    }
+    if($response->success){
+        // Ak reCAPTCHA bola úspešná, pokračujeme s odoslaním e-mailu
+        $to = "jozef.mikus@cpm.sk"; // Sem vložte e-mail príjemcu
+        $subject = "New Contact Form Submission";
+        $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+        $headers = "From: $email";
 
-    // Ak reCAPTCHA bola úspešná, pokračujeme s odoslaním e-mailu
-    $to = "jozef.mikus@cpm.sk"; // Sem vložte e-mail príjemcu
-    $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-    $headers = "From: $email";
-
-    // Pokus o odoslanie e-mailu
-    if (mail($to, $subject, $body, $headers)) {
-        echo "<h2>Form Submitted Successfully!</h2>";
-        echo "<p><strong>Name:</strong> " . $name . "</p>";
-        echo "<p><strong>Email:</strong> " . $email . "</p>";
-        echo "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
+        // Pokus o odoslanie e-mailu
+        if (mail($to, $subject, $body, $headers)) {
+            echo "<h2>Form Submitted Successfully!</h2>";
+            echo "<p><strong>Name:</strong> " . $name . "</p>";
+            echo "<p><strong>Email:</strong> " . $email . "</p>";
+            echo "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
+        } else {
+            echo "There was an error sending the email.";
+            http_response_code(500);
+        }
     } else {
-        echo "There was an error sending the email.";
-        http_response_code(500);
+
+        //neuspesna recaptcha
     }
+
+
 } else {
     echo "Please submit the form.";
 }
