4442fa7ec73c35583b1728352a0c3fdb4e7cdbd0
kabatp <github.tzpmz@passmail.net>
2024-10-05 09:47:49 +0200

    Formating and contact form fix

--- contact.php
+++ contact.php
@@ -71,196 +71,177 @@
 	</head>
 	<body>
 
-	<div class="gtco-loader"></div>
+		<div class="gtco-loader"></div>
 
-	<div id="page">
-	<nav class="gtco-nav" role="navigation">
-		<div class="gtco-container">
-			<div class="row">
-				<div class="col-md-12 text-right gtco-contact">
-					<ul class="">
-						<li><a href="contact.html"><i class="ti-mobile"></i></a></li>
-						<li><a href="https://www.instagram.com/jozefmikus/"><i class="ti-instagram"></i> </a></li>
-						<li><a href="https://www.linkedin.com/in/jozef-miku%C5%A1-150246233/"><i class="ti-linkedin"></i></a></li>
-						<li><a href="https://www.facebook.com/jozko.mikus/"><i class="ti-facebook"></i></a></li>
-					</ul>
+		<div id="page">
+		<nav class="gtco-nav" role="navigation">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-12 text-right gtco-contact">
+						<ul class="">
+							<li><a href="contact.html"><i class="ti-mobile"></i></a></li>
+							<li><a href="https://www.instagram.com/jozefmikus/"><i class="ti-instagram"></i> </a></li>
+							<li><a href="https://www.linkedin.com/in/jozef-miku%C5%A1-150246233/"><i class="ti-linkedin"></i></a></li>
+							<li><a href="https://www.facebook.com/jozko.mikus/"><i class="ti-facebook"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4 col-xs-12">
+						<div id="gtco-logo"><a href="index.html">FINANCE <em>.</em></a></div>
+					</div>
+					<div class="col-xs-8 text-right menu-1">
+						<ul>
+							<li><a href="index.html">Domov</a></li>
+							<li><a href="about.html">Filozofia</a></li>
+							<li class="active"><a href="contact.html">Kontakt</a></li>
+							<li><a href="references.html">Referencie</a></li>
+							<li class="has-dropdown">
+								<a href="#">Služby</a>
+								<ul class="dropdown">
+									<li><a href="osoby.html">Osobné financie</a></li>
+									<li><a href="invest.html">Investície</a></li>
+									<li><a href="firmy.html">Korporátne financie</a></li>
+
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</nav>
+
+		<header id="gtco-header" class="gtco-cover gtco-cover-xs" role="banner" style="background-image:url(images/ja1.jpg);">
+			<div class="overlay"></div>
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+						<div class="display-t">
+							<div class="display-tc">
+								<h1 class="animate-box" data-animate-effect="fadeInUp">Kontakt</h1>
+								<h2 class="animate-box" data-animate-effect="fadeInUp">Your path to prosperity by <em></em> <a href="http://cpm.sk/" target="_blank">www.cpm.sk</a></h2>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-4 col-xs-12">
-					<div id="gtco-logo"><a href="index.html">FINANCE <em>.</em></a></div>
-				</div>
-				<div class="col-xs-8 text-right menu-1">
-					<ul>
-						<li><a href="index.html">Domov</a></li>
-						<li><a href="about.html">Filozofia</a></li>
-						<li class="active"><a href="contact.html">Kontakt</a></li>
-						<li><a href="references.html">Referencie</a></li>
-						<li class="has-dropdown">
-							<a href="#">Služby</a>
-							<ul class="dropdown">
-								<li><a href="osoby.html">Osobné financie</a></li>
-								<li><a href="invest.html">Investície</a></li>
-								<li><a href="firmy.html">Korporátne financie</a></li>
+		</header>
 
-					</ul>
-				</div>
-			</div>
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row row-pb-md">
+					<div class="col-md-6 animate-box">
+					
+						<?php
+							$feedback = "";
 
-		</div>
-	</nav>
+							if(isset($_POST['email'])) {
+								// Získanie hodnôt z formulára
+								$name = htmlspecialchars(($_POST['name']));
+								$email = htmlspecialchars(trim($_POST['email']));
+								$message = htmlspecialchars(trim($_POST['message']));
+								
+								// Kontrola, či sú všetky polia vyplnené
+								if(empty($name) || empty($email) || empty($message)) {
+									$feedback = "All fields are required.";
+									http_response_code(400);
+									exit;
+								}
 
-	<header id="gtco-header" class="gtco-cover gtco-cover-xs" role="banner" style="background-image:url(images/ja1.jpg);">
-		<div class="overlay"></div>
-		<div class="gtco-container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center">
-					<div class="display-t">
-						<div class="display-tc">
-							<h1 class="animate-box" data-animate-effect="fadeInUp">Kontakt</h1>
-							<h2 class="animate-box" data-animate-effect="fadeInUp">Your path to prosperity by <em></em> <a href="http://cpm.sk/" target="_blank">www.cpm.sk</a></h2>
+								// reCAPTCHA secret key a odpoveď od klienta
+								$recaptchaSecret = '6LfXvE8qAAAAAGDNHzk6wDQ8f1PgUzVoq3HkMv9T';
+								$recaptchaResponse = $_POST['recaptchaResponse'];
+
+								// Pošleme požiadavku na Google reCAPTCHA API na overenie
+								$ip = $_SERVER['REMOTE_ADDR'];
+								$url = "https://www.google.com/recaptcha/api/siteverify?secret=".$recaptchaSecret."&response=".$recaptchaResponse."&remoteip=".$ip;
+								$request = file_get_contents(($url));
+								$response = json_decode($request);
+								
+								// Skontrolujeme, či reCAPTCHA bola úspešná
+								if($response->success){
+									// Ak reCAPTCHA bola úspešná, pokračujeme s odoslaním e-mailu
+									$to = "jozef.mikus@cpm.sk"; // Sem vložte e-mail príjemcu
+									$subject = "New Contact Form Submission";
+									$body = "Name: $name\nEmail: $email\nMessage:\n$message";
+									$headers = "From: $email";
+									
+									// Pokus o odoslanie e-mailu
+									if (mail($to, $subject, $body, $headers)) {
+										echo "<h2>Form Submitted Successfully!</h2>";
+										echo "<p><strong>Name:</strong> " . $name . "</p>";
+										echo "<p><strong>Email:</strong> " . $email . "</p>";
+										echo "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
+										$feedback = "Email was successfully sent!";
+									} else {
+										$feedback = "There was an error sending the email, please try again.";
+										http_response_code(500);
+									}
+									} else {
+										
+										//neuspesna recaptcha
+										$feedback = "Please complete the reCAPTCHA verification and try again.";
+								}
+							}
+						?>		
+
+						<div><h1><?php echo $feedback; ?></h1></div>
+
+						<h3>Mám záujem o konzultáciu</h3>
+
+						<form method="post">
+							<input type="hidden" name="recaptchaResponse" id="recaptchaResponse">
+
+							<div class="row form-group">
+								<div class="col-md-12">				
+							<label class="sr-only" for="name">Name:</label>
+							<input type="text" id="name" name="name" class="form-control" placeholder="Celé meno" required><br>
+								</div>	
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12"> 
+							<label class="sr-only" for="email">Email:</label>
+							<input type="email" id="email" name="email" class="form-control" placeholder="E-mail" required><br>
+								</div>
+							</div>
+
+							<div class="row form-group">
+								<div class="col-md-12">
+							<label class="sr-only" for="message">Message:</label>
+							<textarea id="message" name="message" cols="30" rows="10" class="form-control" placeholder="Text správy" required></textarea><br>
+								</div>
+							</div>
+
+							<!-- Google reCAPTCHA widget 
+								<div class="g-recaptcha" data-sitekey="6LfXvE8qAAAAAIQL3S7hcBB8evldl_a4-qB6JZ_4"></div>-->
+
+							<button class="g-recaptcha" type="submit">Odoslať</button>
+
+						</form>
+					</div>
+
+					<div class="col-md-5 col-md-push-1 animate-box">
+						<div class="gtco-contact-info">
+							<h3>Kontaktné informácie</h3>
+							<ul>
+								<li class="address">Trnavská 74/A <br> Bratislava </li>
+								<li class="phone"><a href="tel://915255967">+421 915 255 967</a></li>
+								<li class="email"><a href="jozef.mikus@cpm.sk"> jozef.mikus@cpm.sk</a></li>
+							</ul>
+							
+								<a href="tel:XXX" class="icon-spacing"><i class="ti-mobile"></i></a>
+								<a href="https://www.instagram.com/jozefmikus/" class="icon-spacing"><i class="ti-instagram"></i></a>
+								<a href="contact.html" class="icon-spacing"><i class="icon-mail2"></i></a>
+								<a href="https://www.facebook.com/jozko.mikus/" class="icon-spacing"><i class="ti-facebook"></i></a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</header>
-
-	<div class="gtco-section">
-		<div class="gtco-container">
-			<div class="row row-pb-md">
-				<div class="col-md-6 animate-box">
-				
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Získanie hodnôt z formulára
-    $name = htmlspecialchars(($_POST['name']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $message = htmlspecialchars(trim($_POST['message']));
-
-    // Kontrola, či sú všetky polia vyplnené
-    if (empty($name) || empty($email) || empty($message)) {
-        echo "All fields are required.";
-        http_response_code(400);
-        exit;
-    }
-
-    // reCAPTCHA secret key a odpoveď od klienta
-    $recaptchaSecret = '6LfXvE8qAAAAAGDNHzk6wDQ8f1PgUzVoq3HkMv9T';
-	    $recaptchaResponse = $_POST['recaptchaResponse'];
-
-    // Pošleme požiadavku na Google reCAPTCHA API na overenie
-	    $ip = $_SERVER['REMOTE_ADDR'];
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$recaptchaSecret."&response=".$recaptchaResponse."&remoteip=".$ip;
-    $request = file_get_contents(($url));
-    $response = json_decode($request);
-	
-	 // Skontrolujeme, či reCAPTCHA bola úspešná
-	     if($response->success){
-        // Ak reCAPTCHA bola úspešná, pokračujeme s odoslaním e-mailu
-        $to = "jozef.mikus@cpm.sk"; // Sem vložte e-mail príjemcu
-        $subject = "New Contact Form Submission";
-        $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-        $headers = "From: $email";
-		
-        // Pokus o odoslanie e-mailu
-        if (mail($to, $subject, $body, $headers)) {
-            echo "<h2>Form Submitted Successfully!</h2>";
-            echo "<p><strong>Name:</strong> " . $name . "</p>";
-            echo "<p><strong>Email:</strong> " . $email . "</p>";
-            echo "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
-        } else {
-            echo "There was an error sending the email, please try again.";
-            http_response_code(500);
-        }
-		  } else {
-			  
-			        //neuspesna recaptcha
-					echo "<h2>reCAPTCHA verification failed!</h2>";
-        echo "<p>Please complete the reCAPTCHA verification and try again.</p>";
-    }
-} else {
-    echo "Please submit the form.";
-}
-?>		
-
-		<h3>Mám záujem o konzultáciu</h3>
-
-<form action="" method="post">
-	<input type="hidden" name="recaptchaResponse" id="recaptchaResponse">
-
-		<div class="row form-group">
-			<div class="col-md-12">				
-        <label class="sr-only" for="name">Name:</label>
-        <input type="text" id="name" name="name" class="form-control" placeholder="Celé meno" required><br>
-			</div>	
-		</div>
-
-		<div class="row form-group">
-			 <div class="col-md-12"> 
-        <label class="sr-only" for="email">Email:</label>
-        <input type="email" id="email" name="email" class="form-control" placeholder="E-mail" required><br>
-	     	</div>
-		</div>
-
-	    <div class="row form-group">
-			 <div class="col-md-12">
-        <label class="sr-only" for="message">Message:</label>
-        <textarea id="message" name="message" cols="30" rows="10" class="form-control" placeholder="Text správy" required></textarea><br>
-		     </div>
-		</div>
-
-   <!-- Google reCAPTCHA widget 
-    <div class="g-recaptcha" data-sitekey="6LfXvE8qAAAAAIQL3S7hcBB8evldl_a4-qB6JZ_4"></div>-->
-
-    <button class="g-recaptcha" data-sitekey="6LfXvE8qAAAAAIQL3S7hcBB8evldl_a4-qB6JZ_4" data-callback='onSubmit' type="submit">Odoslať</button>
-
-</form>
-
-<!-- Include the Google reCAPTCHA API 
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>-->
-		</div>
-
-				<div class="col-md-5 col-md-push-1 animate-box">
-
-					<div class="gtco-contact-info">
-						<h3>Kontaktné informácie</h3>
-						<ul>
-							<li class="address">Trnavská 74/A <br> Bratislava </li>
-							<li class="phone"><a href="tel://915255967">+421 915 255 967</a></li>
-							<li class="email"><a href="jozef.mikus@cpm.sk"> jozef.mikus@cpm.sk</a></li>
-							<ul class="">
-
-
-
-<body>
-    <ul>
-        <li>
-<a href="contact.html" class="icon-spacing"><i class="ti-mobile"></i></a>
-<a href="https://www.instagram.com/jozefmikus/" class="icon-spacing"><i class="ti-instagram"></i></a>
-<a href="contact.html" class="icon-spacing"><i class="icon-mail2"></i></a>
-<a href="https://www.facebook.com/jozko.mikus/" class="icon-spacing"><i class="ti-facebook"></i></a>
-
-        </li>
-    </ul>
-</body>
-</html>
-
-					</ul>
-						</ul>
-					</div>
-
-
-				</div>
-			</div>
-			</div>
-
-		</div>
-	</div>
-	</div>
+	</body>
 
     <footer>
-			<div class="overlay"></div>
+		<div class="overlay"></div>
 		<div class="gtco-container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center">
@@ -299,21 +280,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	<script>
         grecaptcha.ready(function(){
-    grecaptcha.execute('6LfXvE8qAAAAAIQL3S7hcBB8evldl_a4-qB6JZ_4', {action:'contactForm'}).then(function(token){
-        document.getElementById('recaptchaResponse').value = token;
-    });
-});
-
-        /*function onSubmit(token) {
-            let recaptchaResponse = document.getElementById("recaptchaResponse")
-            recaptchaResponse.value = token
-            document.getElementById("prihlaskaForm").submit();
-        }
-        function onSubmitKontakt(token) {
-            let recaptchaResponse = document.getElementById("recaptchaResponse")
-            recaptchaResponse.value = token
-           // document.getElementById("contact-form").submit();
-        }*/
+			grecaptcha.execute('6LfXvE8qAAAAAIQL3S7hcBB8evldl_a4-qB6JZ_4', {action:'contactForm'}).then(function(token){
+				document.getElementById('recaptchaResponse').value = token;
+			});
+		});
     </script>
-	</body>
+
 </html>
